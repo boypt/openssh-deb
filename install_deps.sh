bbb064d368acd50ac67040e09876fc08796c056b
Git User <gituser@github.com>
2025-03-13 13:50:29 +0800

    noninterative install

--- install_deps.sh
+++ install_deps.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export DEBIAN_FRONTEND=noninteractive
+
 if [[ ! -z "${APT_MIRROR+x}" ]]; then \
 	[[ -f /etc/apt/sources.list ]] && \
 		sed -i "s|$(awk '/^deb/{print $2}' /etc/apt/sources.list | head -n1 | cut -d/ -f3)|${APT_MIRROR}|" /etc/apt/sources.list
@@ -9,8 +11,10 @@ if [[ ! -z "${APT_MIRROR+x}" ]]; then \
 fi
 
 apt update && apt upgrade -y
-apt install -y sudo pkgconf build-essential fakeroot dpkg-dev debhelper debhelper-compat dh-exec dh-runit \
-	libaudit-dev libedit-dev libgtk-3-dev libselinux1-dev libsystemd-dev libkrb5-dev libpam0g-dev libwrap0-dev
+apt install -y --no-install-recommends sudo pkgconf build-essential fakeroot \
+	dpkg-dev debhelper debhelper-compat dh-exec dh-runit \
+	libaudit-dev libedit-dev libgtk-3-dev libselinux1-dev libsystemd-dev \
+	libkrb5-dev libpam0g-dev libwrap0-dev
 
 if [[ $(apt-cache search --names-only 'libfido2-dev' | wc -l) -gt 0 ]]; then
 	apt install -y libfido2-dev libcbor-dev
