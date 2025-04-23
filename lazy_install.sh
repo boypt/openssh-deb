51f5084a0a52a2b71b866c6f673e12694ff78efe
boypt <1033514+boypt@users.noreply.github.com>
2025-04-23 16:39:18 +0800

    allow run in shell

--- lazy_install.sh
+++ lazy_install.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
-# Bash3 Boilerplate. Copyright (c) 2014, kvz.io
 
 set -o errexit
 set -o pipefail
 set -o nounset
-# set -o xtrace
+set -o xtrace
 
 trap 'echo -e "Aborted, error $? in command: $BASH_COMMAND"; trap ERR; exit 1' ERR
-
-# Set magic variables for current file & dir
-__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
-__base="$(basename ${__file} .sh)"
-__root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on your app
 
 _REPO=boypt/openssh-deb
 USER_REPO="${1:-}"
