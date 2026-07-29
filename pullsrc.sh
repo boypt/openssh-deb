0bee1565b44563d3bdd6dfbf61f74d221a545c32
boypt <1033514+boypt@users.noreply.github.com>
2026-07-29 09:19:46 +0800

    refactor: use brace expansion in DOWNLOADLINKS

--- pullsrc.sh
+++ pullsrc.sh
@@ -18,13 +18,11 @@ arg1="${1:-}"
 
 source $__dir/version.env
 
-DOWNLOADLINKS=( \
-		$DEBMIRROR/pool/main/o/openssh/openssh_${OPENSSH_SIDPKG}.debian.tar.xz \
-		$DEBMIRROR/pool/main/o/openssh/openssh_${OPENSSH_SIDPKG}.dsc \
-		$DEBMIRROR/pool/main/o/openssh/openssh_${OPENSSHVER}.orig.tar.gz \
-		$DEBMIRROR/pool/main/o/openssh/openssh_${OPENSSHVER}.orig.tar.gz.asc \
-		${OPENSSLMIR}/${OPENSSLSRC}
-	      )
+DOWNLOADLINKS=(
+	$DEBMIRROR/pool/main/o/openssh/openssh_${OPENSSH_SIDPKG}.{debian.tar.xz,dsc}
+	$DEBMIRROR/pool/main/o/openssh/openssh_${OPENSSHVER}.orig.tar.gz{,.asc}
+	${OPENSSLMIR}/${OPENSSLSRC}
+)
 
 mkdir -p $__dir/downloads && cd $__dir/downloads
 echo "> INFO: downloading the following sources."
